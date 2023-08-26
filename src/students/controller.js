01af88b6b964f0555ca754204c405013b7473947
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-25 20:16:10 -0600

    new updates

--- src/students/controller.js
+++ src/students/controller.js
@@ -6,8 +6,17 @@ const getStudents = (req, res) => {
         if(error) throw error;
         res.status(200).json(results.rows);
     });
+
 };
 
+const getStudentById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getStudentById, [id], (error, results) => {
+            if(error) throw error;
+            res.status(200).json(results.rows);});
+}
+
 module.exports = {
-    getStudents
+    getStudents,
+    getStudentById
 };
